3d23fdfbdbba95c6dc965e59b9e9a1489ffe073a
LLBGS <lucasllbgs@gmail.com>
2023-05-24 14:44:43 -0300

    feat: app config

--- src/configs/environment/app.config.ts
+++ src/configs/environment/app.config.ts
@@ -9,7 +9,8 @@ interface IAppConfig {
 
 export class AppConfig {
   private static requiredEnvs = ['PORT', 'NODE_ENV'];
-  private static _environment: IAppConfig | undefined;
+  private static _environment: IAppConfig;
+  private static environmentNames = ['development', 'production', 'qas', 'uat'];
 
   static get environment(): IAppConfig {
     if (typeof this._environment === 'undefined') {
@@ -21,17 +22,46 @@ export class AppConfig {
 
   static validateIfRequiredEnvsArePresent() {
     this.requiredEnvs.forEach((env) => {
-      if (!process.env[env]) {
+      if (typeof process.env[env] === 'undefined') {
         throw new Error(`Environment variable ${env} is missing`);
       }
     });
   }
 
   static mountEnvironment(): IAppConfig {
+    const port = this.validatePort(Number(process.env.PORT));
+    const nodeEnv = this.validateNodeEnv(String(process.env.NODE_ENV));
+    const appName = this.validateAppName(String(process.env.APP_NAME));
+
     return {
-      port: Number(process.env.PORT) || 3000,
-      nodeEnv: process.env.NODE_ENV || 'development',
-      appName: process.env.APP_NAME || 'app',
+      port,
+      nodeEnv,
+      appName,
     };
+  }
+
+  private static validateAppName(appName: string): string {
+    if (appName === 'undefined' || appName === '') {
+      throw new Error(
+        `Invalid value for APP_NAME environment variable: ${process.env.APP_NAME}`,
+      );
+    }
+    return appName;
+  }
+
+  private static validatePort(port: number): number {
+    if (isNaN(port) || port <= 0) {
+      throw new Error(`Invalid value for PORT environment variable: ${process.env.PORT}`);
+    }
+    return port;
+  }
+
+  private static validateNodeEnv(nodeEnv: string): string {
+    if (!this.environmentNames.includes(nodeEnv)) {
+      throw new Error(
+        `Invalid value for NODE_ENV environment variable: ${process.env.NODE_ENV}`,
+      );
+    }
+    return nodeEnv;
   }
 }
